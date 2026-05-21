54f99a6aa7a14443d5b26ec503541c36ce333196
quinniezip <trannhuquynh@phuctea.com.vn>
2026-05-21 11:32:14 +0700

    Update reportProgress.js

--- src/utils/reportProgress.js
+++ src/utils/reportProgress.js
@@ -1,5 +1,5 @@
 // Paste your Apps Script Web App URL here after deploying
-const SHEETS_URL = '';
+const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzAm5y0c5ASvRAc0-BvuWGJWJ2ikz6aLOAf9l4UMDUR4ep__NyRtrBreP-1rFWM4CKTSA/exec';
 
 export function reportProgress(data) {
   if (!SHEETS_URL) return;
